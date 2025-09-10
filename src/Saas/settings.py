c251b57c40c40adc1b68e656bbea3f32ad74ab10
ismailrahioui <ismail.0rahiouy@gmail.com>
2025-09-10 15:19:20 +0100

    Perapare for Railway

--- src/Saas/settings.py
+++ src/Saas/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-30tem98mtm%fb^6h&b7zx0w9xg0l29*33^l35c54d$$!t+5u&a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower == "true"
+
+print("DEBUG",DEBUG,type(DEBUG))
 
 ALLOWED_HOSTS = [
     ".railway.app"
