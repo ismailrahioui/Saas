8630e1135c7eb2d6d642001f46e551a6f192f7ce
ismailrahioui <ismail.0rahiouy@gmail.com>
2025-09-11 13:26:26 +0100

    Prepare for Railway

--- src/Saas/settings.py
+++ src/Saas/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-import os
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-30tem98mtm%fb^6h&b7zx0w9xg0l29*33^l35c54d$$!t+5u&a'
-
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower == "true"
+#DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower == "true"
+DEBUG=config("DJANGO_DEBUG",cast=bool)
+
 
 print("DEBUG",DEBUG,type(DEBUG))
 
@@ -85,11 +86,20 @@ WSGI_APPLICATION = 'Saas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+
+
+CONN_MAX_AGE=config("CONN_MAX_AGE",cast=int,default=30)
+DATABASE_URL=config("DATABASE_URL",cast=str)
+
+if DATABASE_URL is not None:
+    import dj_database_url
+    DATABASES = {
+    'default': dj_database_url.config(
+        default=DATABASE_URL,
+        conn_max_age=CONN_MAX_AGE,
+        conn_health_checks=True,
+
+    )
 }
 
 
